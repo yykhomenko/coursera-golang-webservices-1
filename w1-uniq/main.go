ba0159a8d08ea810d5643666223dedf5df7be0e8
yykhomenko <yykhomenko@gmail.com>
2020-06-20 10:12:41 +0300

    refactoring main.go

--- w1-uniq/main.go
+++ w1-uniq/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
-func main() {
-	in := bufio.NewScanner(os.Stdin)
+func uniq(r io.Reader, w io.Writer) error {
+	in := bufio.NewScanner(r)
 	var prev string
 
 	for in.Scan() {
@@ -18,11 +19,20 @@ func main() {
 		}
 
 		if txt < prev {
-			panic("file not sorted")
+			return fmt.Errorf("file not sorted")
 		}
 
 		prev = txt
 
-		fmt.Println(txt)
+		fmt.Fprintln(w, txt)
+	}
+
+	return nil
+}
+
+func main() {
+	err := uniq(os.Stdin, os.Stdout)
+	if err != nil {
+		panic(err.Error())
 	}
 }
