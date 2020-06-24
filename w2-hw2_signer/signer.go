8621fdad247975c25839151f2cca74f1d8790b00
yykhomenko <yykhomenko@gmail.com>
2020-06-24 22:24:12 +0300

    w2 added func SingleHash

--- w2-hw2_signer/signer.go
+++ w2-hw2_signer/signer.go
@@ -1,3 +1,13 @@
 package main
 
-// сюда писать код
+func ExecutePipeline(jobs ...job) {
+
+}
+
+func SingleHash(data string) string {
+
+	data = "string"
+	r := DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data))
+
+	return r
+}
